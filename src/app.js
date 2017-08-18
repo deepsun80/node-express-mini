6692dd08af4d2df639c45c2ffd5c5afa6ac88ade
Sandeep Chandran <deepsun80@yahoo.com>
2017-08-18 11:09:32 -0500

    Working Hangman Node Game

--- src/app.js
+++ src/app.js
@@ -13,7 +13,60 @@ const readWords = () => {
   const contents = fs.readFileSync('words.txt', 'utf8');
   return contents.split('\n');
 };
+const wordsArray = readWords();
+const word = wordsArray[Math.floor(Math.random() * wordsArray.length)].toLowerCase();
+
+const wordArray = [];
 
 // TODO: your code to handle requests
+//GET
+server.get('/guess', (req, res) => {
+  const wordSoFar = [];
+  if (wordArray.length === 0) {
+    res.status(STATUS_USER_ERROR);
+    res.json({ error: 'Must provide a letter' });
+    return;
+  }
+  if (wordArray.length > 5) {
+    res.status(STATUS_USER_ERROR);
+    res.send(`Sorry, too many guesses, the correct answer is ${word}`);
+    return;
+  }
+  for (let i = 0; i < word.length - 1; i++) {
+    wordSoFar.push('-');
+    wordArray.forEach((element) => {
+      if (element === word[i]) {
+        wordSoFar.splice(i, 1, word[i]);
+      }
+    });
+  }
+  const wordSoFarToString = wordSoFar.join('');
+  const guessesSoFar = wordArray.toString();
+  //console.log(word);
+  //console.log(wordArray);
+  res.json({ 
+    wordSoFar: wordSoFarToString,
+    guesses: guessesSoFar
+  });
+});
+
+//POST
+server.post('/guess', (req, res) => {
+  const letter = req.body.letter;
+  if (!letter) {
+    res.status(STATUS_USER_ERROR);
+    res.json({ error: 'Must provide a letter' });
+    return;
+  }
+  wordArray.forEach((element) => {
+    if (letter === element) {
+      res.status(STATUS_USER_ERROR);
+      res.json({ error: 'Must provide a different letter' });
+      return;
+    }
+  });
+  wordArray.push(letter);
+  res.json({ wordArray });
+});
 
 server.listen(3000);
